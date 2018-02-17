530e050a7851620833f00e324ede9b26750d842f
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-02-16 17:52:36 -0800

    - deploy only happens on python 3.5 now - bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Painless-SQLAlchemy',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/GetintheLoop/painless-sqlalchemy',
     license='MIT',
     author='Lukas Siemon',
