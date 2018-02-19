dd17adfea2fdddb6e7112fdf4926ff9a8b70d1d3
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-02-19 14:59:33 -0800

    - fixed comment format

--- tests/abstract/DBIdMixin.py
+++ tests/abstract/DBIdMixin.py
@@ -6,10 +6,11 @@ class DBIdMixin(object):
 
     @classmethod
     def extract(cls, *args, **kwargs):
-        """ Extract ids for passed Models recursively
-        :param args Starting Models to extract ids from
-        :param kwargs Pass in ignore=tuple(table name, ...) to skip tables
-        :return dict mapping {table name -> tuple of ids}
+        """
+            Extract ids for passed Models recursively
+            :param args Starting Models to extract ids from
+            :param kwargs Pass in ignore=tuple(table name, ...) to skip tables
+            :return dict mapping {table name -> tuple of ids}
         """
         ignore = kwargs.get('ignore', set())
         result = {}
