108894fd310a8dc8d0514e0d89b50a41b0c2b7d3
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-02-19 11:22:29 -0800

    - added "AbstractExtendedTest" and "DBIdMixin"

---
+++ tests/abstract/AbstractExtendedTest.py
@@ -0,0 +1,39 @@
+from collections import namedtuple
+from sqlalchemy.orm import class_mapper, ColumnProperty
+from painless_sqlalchemy.Model import Model
+
+
+class AbstractExtendedTest(object):
+    @staticmethod
+    def get(list_, key_, value_):
+        """
+            Find first dict/object in list_ where dict/object[key_] == value_
+            - key_ assumed present in all dicts/objects
+            - value_ and values are cast to strings before comparison
+            :param list_ List of dicts/objects
+            :param key_ Key name to use in search
+            :param value_ Value of key tp search for
+            :return first dict/object if found, otherwise None
+        """
+        if any(hasattr(o, '__getitem__') for o in list_):
+            for o in list_:
+                if str(o[key_]) == str(value_):
+                    return o
+        else:
+            for o in list_:
+                if str(getattr(o, key_)) == str(value_):
+                    return o
+        return None
+
+    @staticmethod
+    def persist(obj):
+        """ Clone model top-level fields into SQLAlchemy-independent object """
+        # todo: only use fields that are loaded (prevent additional queries)
+        assert isinstance(obj, Model)
+        keys = [
+            prop.key for prop in class_mapper(obj.__class__).iterate_properties
+            if isinstance(prop, ColumnProperty)
+        ]
+        return namedtuple(obj.__class__.__tablename__, ' '.join(keys))(
+            **{key: getattr(obj, key) for key in keys}
+        )
---
+++ tests/abstract/DBIdMixin.py
@@ -0,0 +1,54 @@
+from sqlalchemy import inspect
+from painless_sqlalchemy.Model import Model
+
+
+class DBIdMixin(object):
+
+    @classmethod
+    def extract(cls, *args, **kwargs):
+        """ Extract ids for passed Models recursively
+        :args Starting Models to extract ids from
+        :kwargs Pass in ignore=tuple(table name, ...) to skip tables
+        :return dict mapping {table name -> tuple of ids}
+        """
+        ignore = kwargs.get('ignore', set())
+        result = {}
+
+        # handle id extraction
+        to_ignore = set()
+        for arg in args:
+            assert isinstance(arg, Model)
+            if arg.__table__ is not None:
+                name = arg.__table__.name
+                to_ignore.add(name)
+                if name not in result:
+                    result[name] = set()
+                assert arg.id is not None
+                result[name].add(arg.id)
+
+        ignore = ignore.union(to_ignore)
+
+        # handle relationships
+        for arg in args:
+            for rel_name in inspect(arg.__class__).relationships.keys():
+                rel = getattr(arg.__class__, rel_name)
+                name = rel.property.mapper.class_.__table__.name
+                if name in ignore:
+                    continue
+                rel_target = getattr(arg, rel_name)
+                if rel_target is None:
+                    continue
+                if not isinstance(rel_target, list):
+                    rel_target = [rel_target]
+                if not len(rel_target):
+                    continue
+                for name, ids in cls.extract(
+                    *rel_target,
+                    **{"ignore": ignore}
+                ).items():
+                    if name not in result:
+                        result[name] = set()
+                    result[name] = result[name].union(ids)
+        return {
+            k: tuple(set(v)) for k, v in result.items()
+        }
