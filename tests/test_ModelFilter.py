bbec799a19c79c8bf2b98c29de5f70b4d7a53be1
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-19 11:22:56 -0800

    Added cleanup to test Added coverage tests for skip_nones / literal column query for filter

--- tests/test_ModelFilter.py
+++ tests/test_ModelFilter.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy import literal_column as lc, and_
 from sqlalchemy.orm import sessionmaker
 from painless_sqlalchemy.BaseModel import engine
 
@@ -9,7 +10,7 @@ class TestModelFilter(object):
     @pytest.fixture(scope='class', autouse=True)
     def setup(cls, School, Classroom, Teacher, Student):
         student1 = Student(name='foo')
-        student2 = Student(name='baz')
+        student2 = Student(name='bar', address='baz')
 
         teacher = Teacher(students=[student1, student2])
         classroom = Classroom(teacher=teacher)
@@ -27,6 +28,15 @@ class TestModelFilter(object):
         cls.teacher_id = teacher.id
         cls.classroom_id = classroom.id
         cls.school_id = school.id
+
+        yield  # run tests in class
+        # cleanup
+        session = sessionmaker(engine)()
+        session.query(Student).delete()
+        session.query(Teacher).delete()
+        session.query(Classroom).delete()
+        session.query(School).delete()
+        session.commit()
 
     def test_filter_by_id(self, Teacher):
         assert Teacher.filter({
@@ -64,3 +74,32 @@ class TestModelFilter(object):
         }).all()
         assert len(classrooms) == 1
         assert classrooms[0].id == self.classroom_id
+
+    def test_filter_by_literal_column(self, Student):
+        student = Student.filter(
+            and_(*[
+                lc('id') == self.student1['id'],
+                lc('name') == self.student1['name']
+            ])
+        ).one()
+        assert student.id == self.student1['id']
+
+    def test_filter_skip_nones(self, Student):
+        student = Student.filter({
+            'id': self.student2_id,
+            'address': None
+        }, skip_nones=True).one()
+        assert student.id == self.student2_id
+
+    def test_filter_for_null_value(self, Student):
+        student = Student.filter({
+            'id': self.student1['id'],
+            'address': None
+        }).first()
+        assert student is not None
+
+        student = Student.filter({
+            'id': self.student2_id,
+            'address': None
+        }).first()
+        assert student is None
