fcc4e2485c98fdf1cf52a3d9fee491fabd73228d
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-19 16:57:28 -0800

    Made setup_method function scoped test_ModelAction now uses setup_class

--- tests/test_ModelAction.py
+++ tests/test_ModelAction.py
@@ -1,9 +1,24 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
 from tests.abstract.AbstractDatabaseTest import AbstractDatabaseTest
+from faker import Faker
+
+fake = Faker()
 
 
 class TestModelAction(AbstractDatabaseTest):
+
+    @classmethod
+    @pytest.fixture(scope='class', autouse=True)
+    def setup_class(cls, Teacher, Student):
+        super(TestModelAction, cls).setup_class()
+        student = Student(name=fake.name())
+        teacher = Teacher()
+
+        cls.checkin(student, teacher)
+
+        cls.student = cls.persist(student)
+        cls.teacher = cls.persist(teacher)
 
     def test_get_session(self, Student):
         """ Test session always exists """
@@ -14,6 +29,7 @@ class TestModelAction(AbstractDatabaseTest):
         teacher_id = Teacher().save().id
         teacher = Teacher.filter({'id': teacher_id}).one()
         assert teacher.id == teacher_id
+        self.register(teacher=teacher_id)
 
     def test_delete(self, Student):
         student_id = Student(name='foo').save().id
@@ -22,19 +38,30 @@ class TestModelAction(AbstractDatabaseTest):
         assert Student.filter({'id': student_id}).first() is None
 
     def test_update(self, Student):
-        student_id = Student(name='foo').save().id
-        assert Student.filter({'id': student_id}).one().name == 'foo'
-        Student.filter({'id': student_id}).one().update(name='bar').save()
-        assert Student.filter({'id': student_id}).one().name == 'bar'
+        assert Student.filter({
+            'id': self.student.id
+        }).one().name == self.student.name
+        new_name = fake.name()
+        Student.filter({
+            'id': self.student.id
+        }).one().update(name=new_name).save()
+        assert Student.filter({'id': self.student.id}).one().name == new_name
+
+        # revert
+        Student.filter({
+            'id': self.student.id
+        }).one().update(name=self.student.name).save()
 
     def test_update_invalid_column(self, Student):
         with pytest.raises(AttributeError):
             Student().update(invalid_column=None)
 
     def test_rollback(self, Student):
-        student = Student(name='foo').save()
+        student = Student.filter({'id': self.student.id}).one()
         student.update(name=None)
         with pytest.raises(IntegrityError):
             student.save()
         student.rollback()
-        assert Student.filter({'id': student.id}).one().name == 'foo'
+        assert Student.filter({
+            'id': student.id
+        }).one().name == self.student.name
