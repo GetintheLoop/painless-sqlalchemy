34f1222bcf2787350745eee48729153b846cdf85
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-20 12:45:54 -0800

    Added map_column coverage

--- tests/test_ModelSerialization.py
+++ tests/test_ModelSerialization.py
@@ -14,7 +14,9 @@ class TestModelSerialization(AbstractDatabaseTest):
     @pytest.fixture(scope='class', autouse=True)
     def setup_class(cls, School, Classroom, Teacher, Student):
         super(TestModelSerialization, cls).setup_class()
-        student1 = Student(name=fake.name())
+        student1 = Student(
+            name=fake.name(), phone=fake.phone_number(),
+            home_phone=fake.phone_number(), email=fake.email())
         student2 = Student(name=fake.name(), address=fake.address())
         teacher = Teacher(students=[student1, student2])
         classroom = Classroom(teacher=teacher)
@@ -22,10 +24,7 @@ class TestModelSerialization(AbstractDatabaseTest):
 
         cls.checkin(student1, student2, teacher, classroom, school)
 
-        cls.student1 = {
-            'id': student1.id,
-            'name': student1.name
-        }
+        cls.student1 = cls.persist(student1)
         cls.student2_id = student2.id
         cls.teacher_id = teacher.id
         cls.school_id = school.id
@@ -43,13 +42,13 @@ class TestModelSerialization(AbstractDatabaseTest):
         student = Student.serialize(
             to_return=['id'],
             filter_by=and_(*[
-                ref('id') == self.student1['id'],
-                ref('name') == self.student1['name']
+                ref('id') == self.student1.id,
+                ref('name') == self.student1.name
             ]),
             suppress=False
         )
         assert len(student) == 1
-        assert student[0]['id'] == self.student1['id']
+        assert student[0]['id'] == self.student1.id
 
     def test_skip_nones(self, Student):
         student = Student.serialize(
@@ -72,7 +71,7 @@ class TestModelSerialization(AbstractDatabaseTest):
 
         schools = School.serialize(
             to_return=['id', 'classrooms.teacher.students.id'],
-            filter_by={'classrooms.teacher.students.id': self.student1['id']},
+            filter_by={'classrooms.teacher.students.id': self.student1.id},
             suppress=True
         )
         assert len(schools) == 1
@@ -80,8 +79,44 @@ class TestModelSerialization(AbstractDatabaseTest):
 
         schools = School.serialize(
             to_return=['id', 'classrooms.teacher.students.id'],
-            filter_by={'classrooms.teacher.students.id': self.student1['id']},
+            filter_by={'classrooms.teacher.students.id': self.student1.id},
             suppress=False
         )
         assert 'id' in schools[0]
         assert 'classrooms.teacher.students.id' in flatten_dict(schools)
+
+    def test_serialize_map_column_dict(self, Student):
+        student = Student.serialize(
+            to_return=['contact_info.*'],
+            filter_by={'id': self.student1.id}
+        )
+        assert len(student) == 1
+        assert student[0] == {
+            'contact_info': {
+                'phone': self.student1.phone,
+                'home_phone': self.student1.home_phone,
+                'email': self.student1.email
+            }
+        }
+
+    def test_serialize_map_column_list(self, Student):
+        student = Student.serialize(
+            to_return=['phone_numbers'],
+            filter_by={'id': self.student1.id}
+        )
+        assert len(student) == 1
+        assert student[0] == {
+            'phone_numbers': [self.student1.phone, self.student1.home_phone]
+        }
+
+    def test_serialize_map_column_dict_subset(self, Student):
+        student = Student.serialize(
+            to_return=['contact_info.phone'],
+            filter_by={'id': self.student1.id}
+        )
+        assert len(student) == 1
+        assert student[0] == {
+            'contact_info': {
+                'phone': self.student1.phone
+            }
+        }
