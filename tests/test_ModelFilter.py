70686e4f7f11fde02dd03ef8e7c9f7559a95e280
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-21 11:26:47 -0800

    Added second teacher / classroom Added test that filtering on *_to_one relationship results in "OR" query

--- tests/test_ModelFilter.py
+++ tests/test_ModelFilter.py
@@ -17,55 +17,62 @@ class TestModelFilter(AbstractDatabaseTest):
         student2 = Student(name=fake.name(), address=fake.address())
         student3 = Student(name=fake.name(), address=fake.address())
 
-        teacher = Teacher(name=fake.name(), students=[student1, student2])
-        classroom = Classroom(teacher=teacher)
-        school = School(classrooms=[classroom])
+        teacher1 = Teacher(name=fake.name(), students=[student1, student2])
+        teacher2 = Teacher(name=fake.name())
+        classroom1 = Classroom(teacher=teacher1)
+        classroom2 = Classroom(teacher=teacher2)
+        school = School(classrooms=[classroom1, classroom2])
 
-        cls.checkin(student1, student2, student3, teacher, classroom, school)
+        cls.checkin(
+            student1, student2, student3,
+            teacher1, teacher2,
+            classroom1, classroom2,
+            school
+        )
 
         cls.student1 = cls.persist(student1)
         cls.student2 = cls.persist(student2)
         cls.student3 = cls.persist(student3)
-        cls.teacher = cls.persist(teacher)
-        cls.classroom = cls.persist(classroom)
+        cls.teacher1 = cls.persist(teacher1)
+        cls.teacher2 = cls.persist(teacher2)
+        cls.classroom1 = cls.persist(classroom1)
         cls.school = cls.persist(school)
 
     def test_filter_by_id(self, Teacher):
         assert Teacher.filter({
-            'id': self.teacher.id
-        }).one().id == self.teacher.id
+            'id': self.teacher1.id
+        }).one().id == self.teacher1.id
 
     def test_filter_by_many_to_many_relationship(self, Teacher):
         assert Teacher.filter({
             'students.name': self.student1.name
-        }).one().id == self.teacher.id
+        }).one().id == self.teacher1.id
 
     def test_filter_by_one_to_one_relationship(self, Classroom):
         assert Classroom.filter({
-            'teacher.id': self.teacher.id
-        }).one().id == self.classroom.id
+            'teacher.id': self.teacher1.id
+        }).one().id == self.classroom1.id
 
     def test_filter_by_foreign_key(self, Teacher):
         assert Teacher.filter({
-            'classroom_id': self.classroom.id
-        }).one().id == self.teacher.id
+            'classroom_id': self.classroom1.id
+        }).one().id == self.teacher1.id
 
     def test_filter_by_nested_relationship(self, School):
         assert School.filter({
-            'classrooms.teacher.id': self.teacher.id
+            'classrooms.teacher.id': self.teacher1.id
         }).one().id == self.school.id
 
     def test_filter_one_to_many_relationship(self, School):
         assert School.filter({
-            'classrooms.id': self.classroom.id
+            'classrooms.id': self.classroom1.id
         }).one().id == self.school.id
 
     def test_filter_many_to_one_relationship(self, Classroom):
         classrooms = Classroom.filter({
             'school.id': self.school.id
         }).all()
-        assert len(classrooms) == 1
-        assert classrooms[0].id == self.classroom.id
+        assert len(classrooms) == 2
 
     def test_ref_filter(self, Student):
         student = Student.filter(
@@ -112,12 +119,17 @@ class TestModelFilter(AbstractDatabaseTest):
             'id': [self.student1.id]
         }).one().id == self.student1.id
 
-    def test_filter_list_column_or_query(self, Student):
+    def test_filter_list_is_or_query_for_column(self, Student):
         assert len(Student.filter({
             'id': [self.student1.id, self.student2.id]
         }).all()) == 2
 
-    def test_filter_relationship_and_query(self, Teacher):
+    def test_filter_list_is_or_query_for_relationship_to_one(self, Classroom):
+        assert len(Classroom.filter({
+            'teacher.id': [self.teacher1.id, self.teacher2.id]
+        }).all()) == 2
+
+    def test_filter_list_is_and_query_for_relationship_to_many(self, Teacher):
         assert Teacher.filter({
             'students.id': [self.student1.id, self.student2.id]
         }).first() is not None
@@ -130,6 +142,6 @@ class TestModelFilter(AbstractDatabaseTest):
 
     def test_filter_by_empty_list_relationship(self, Teacher):
         assert Teacher.filter({
-            'id': self.teacher.id,
+            'id': self.teacher1.id,
             'students.id': []
-        }).one().id == self.teacher.id
+        }).one().id == self.teacher1.id
