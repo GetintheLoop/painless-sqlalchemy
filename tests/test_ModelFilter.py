4977a7d6167b730886b021ead3bf25d4fb8eb5eb
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-21 17:00:58 -0800

    Added test for filter by case

--- tests/test_ModelFilter.py
+++ tests/test_ModelFilter.py
@@ -1,6 +1,8 @@
 import pytest
-from sqlalchemy import and_, or_, func
+from sqlalchemy import and_, or_, func, case
 from faker import Faker
+from sqlalchemy.dialects.postgresql import INTERVAL
+
 from painless_sqlalchemy.elements.ColumnReference import ref
 from tests.helper.AbstractDatabaseTest import AbstractDatabaseTest
 
@@ -161,3 +163,16 @@ class TestModelFilter(AbstractDatabaseTest):
         assert Student.filter(
             ref('created') <= func.now()
         ).first() is not None
+
+    def test_filter_ref_case(self, Student):
+        assert len(Student.filter(
+            case([(
+                ref('created') <=
+                func.now() - func.cast('2 YEARS', INTERVAL),
+                'senior'
+            ), (
+                ref('created') <=
+                func.now() - func.cast('1 YEAR', INTERVAL),
+                'junior'
+            )], else_='freshman') == 'freshman'
+        ).all()) == 3
