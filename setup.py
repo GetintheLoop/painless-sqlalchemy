9a496d6faec5102d845f9b0ebd1cd88311e89d59
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-02-22 10:22:22 -0800

    - set version to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Painless-SQLAlchemy',
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/GetintheLoop/painless-sqlalchemy',
     license='MIT',
     author='Lukas Siemon',
