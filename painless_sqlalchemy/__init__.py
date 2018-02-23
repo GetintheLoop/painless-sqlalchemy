265a1227d1e9d87c2db4d31c835684ff87de0407
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-23 14:16:25 -0800

    Added session_options to Painless.__init__

--- painless_sqlalchemy/__init__.py
+++ painless_sqlalchemy/__init__.py
@@ -5,11 +5,13 @@ from painless_sqlalchemy.core.Model import Model
 
 class Painless(object):
 
-    def __init__(self, db_uri):
+    def __init__(self, db_uri, session_options=None):
+        if session_options is None:
+            session_options = {}
         self.db_info = db_uri
         self.engine = create_engine(db_uri)
         self.session = scoped_session(
-            sessionmaker(autocommit=False, bind=self.engine))
+            sessionmaker(bind=self.engine, **session_options))
 
         Model.query = self.session.query_property()
         Model.session = self.session
