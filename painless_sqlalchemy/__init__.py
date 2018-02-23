63b6265f1e5693011e30f742b8220a85878da8c8
Jeff Soloshy <jeff.soloshy@getintheloop.ca>
2018-02-23 14:09:36 -0800

    Removed extra comments

--- painless_sqlalchemy/__init__.py
+++ painless_sqlalchemy/__init__.py
@@ -5,11 +5,6 @@ from painless_sqlalchemy.core.Model import Model
 
 class Painless(object):
 
-    # excluded tables, e.g. used for postgres spatial extension (PostGIS)
-
-    version_table = ['alembic_version']
-    _known_session = set()
-
     def __init__(self, db_uri):
         self.db_info = db_uri
         self.engine = create_engine(db_uri)
