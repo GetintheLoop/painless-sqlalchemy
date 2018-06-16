abc63d76f2e2da4c5b54dbbd02c725951187e6af
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-06-15 20:47:54 -0700

    amend(fix): changed rollback test amend(fix): changed gardener docker version to use postgis

--- tests/core/test_ModelAction.py
+++ tests/core/test_ModelAction.py
@@ -56,12 +56,12 @@ class TestModelAction(AbstractTest):
         with pytest.raises(AttributeError):
             Student().update(invalid_column=None)
 
-    def test_rollback(self, Student):
-        student = Student.filter({'id': self.student.id}).one()
-        student.update(name=None)
+    def test_rollback(self, Teacher):
+        teacher = Teacher.filter({'id': self.teacher.id}).one()
+        teacher.update(classroom_id=-1)
         with pytest.raises(IntegrityError):
-            student.save()
-        student.rollback()
-        assert Student.filter({
-            'id': student.id
-        }).one().name == self.student.name
+            teacher.save()
+        teacher.rollback()
+        assert Teacher.filter({
+            'id': teacher.id
+        }).one().classroom_id == self.teacher.classroom_id
