30c6332c7bdca82c96990c2013d2863b77638591
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-06-15 20:30:20 -0700

    feat: added custom column types CIText and ColorType chore: tests now install a bunch of extensions (e.g. citext)

---
+++ tests/columns/test_ColorType.py
@@ -0,0 +1,32 @@
+import pytest
+from faker import Faker
+from tests.AbstractTest import AbstractTest
+
+fake = Faker()
+
+
+class TestColorType(AbstractTest):
+
+    @classmethod
+    @pytest.fixture(scope='class', autouse=True)
+    def setup_class(cls, School, Classroom):
+        super(TestColorType, cls).setup_class()
+        school = School()
+        classroom = Classroom(school=school)
+
+        cls.checkin(school, classroom)
+
+        cls.classroom = cls.persist(classroom)
+
+    def test_storage(self, Classroom):
+        color = "#007700"
+        classroom = Classroom.filter().one()
+        classroom.update(color=color).save()
+        classroom = Classroom.filter().one()
+        assert classroom.color == color
+        classroom.update(color=None).save()
+
+    def test_invalid(self, Classroom):
+        classroom = Classroom.filter().one()
+        with pytest.raises(ValueError):
+            classroom.update(color="#77777777").save()
